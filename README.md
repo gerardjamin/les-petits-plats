4fdd05fe6acfdf2ab0d15917a5f72b3eb4ba3f61
jamin <gerard.jamin@orange.fr>
2023-12-14 09:34:40 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# Project Title
+
+LES PETITS PLATS est un site de recettes de cuisine.
+Interface web avec son moteur de recherche articulé sur les commandes natives for().
+
+
+## Authors
+
+- [@gerardjamin]( https://github.com/gerardjamin/les-petits-plats.git)
+
+
+## 🚀 About Me
+Je suis étudiant chez OpenClassRoom et ce projet est le numero 7
+
+
+## Roadmap
+
+- debut de projet: 06/11/2023
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    // Ajoutez d'autres chemins de fichiers si nécessaire
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'image':'url("/assets/background-image.jpg")',
+        'loop':'url("/assets/LogoLoupe.jpg")',
+        'loopHover':'url("/assets/LogoLoupeHover.jpg")',
+        'loopInput':'url("/assets/loupe-input.jpg")',
+        'croix':'url("/assets/croix.jpg")',
+        'croixTag':'url("/assets/croix.svg")',
+      },
+      height: {
+        '667': '667px',
+      },
+      width: {
+        '1440': '1440px',
+        '954':'954px',
+      },
+      fontFamily: {
+        anton: "Anton",
+        manrope:"Manrope",
+      },
+    },
+  },
+  plugins: [],
+}
+
