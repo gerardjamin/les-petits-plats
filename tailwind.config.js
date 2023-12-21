6eddb7b2eb17ecb4ec9c6b4579a894d4da689cd7
jamin <99016496+gerardjamin@users.noreply.github.com>
2023-12-21 08:32:18 +0100

    Update tailwind.config.js
    modification URL des  icones

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'image':'url("./assets/background-image.jpg")',
-        'loop':'url("./assets/LogoLoupe.jpg")',
-        'loopHover':'url("./assets/LogoLoupeHover.jpg")',
-        'loopInput':'url("./assets/loupe-input.jpg")',
-        'croix':'url("./assets/croix.jpg")',
-        'croixTag':'url("./assets/croix.svg")',
+        'image':'url("/les-petits-plats/assets/background-image.jpg")',
+        'loop':'url("/les-petits-plats/assets/LogoLoupe.jpg")',
+        'loopHover':'url("/les-petits-plats/assets/LogoLoupeHover.jpg")',
+        'loopInput':'url("/les-petits-plats/assets/loupe-input.jpg")',
+        'croix':'url("/les-petits-plats/assets/croix.jpg")',
+        'croixTag':'url("/les-petits-plats/assets/croix.svg")',
       },
       height: {
         '667': '667px',
